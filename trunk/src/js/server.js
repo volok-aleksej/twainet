a1ad7aa0835041cddbad4e4c927fce482fcb0c39
volokaleks@yandex.ru <volokaleks@yandex.ru@25897825-9b42-e7f4-afcc-2c1af6338310>
2014-02-18 17:12:46 +0000

    ipc module - connector

--- trunk/src/js/server.js
+++ trunk/src/js/server.js
@@ -2,6 +2,7 @@ var module = require('clientServerModule');
 
 var server = new module.ClientServerModule("MainServer");
 server.StartServer(8124);
+server.StartAsCoordinator();
 
 process.on('uncaughtException', function (err) {
   console.error(err.stack);
