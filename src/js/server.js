34ec72b09c5f9f72d41051a1ddbc49ad7990ed6b
volokaleks@yandex.ru <volokaleks@yandex.ru@25897825-9b42-e7f4-afcc-2c1af6338310>
2014-02-18 13:02:25 +0000

    fix crash node

--- src/js/server.js
+++ src/js/server.js
@@ -2,3 +2,7 @@ var module = require('clientServerModule');
 
 var server = new module.ClientServerModule("MainServer");
 server.StartServer(8124);
+
+process.on('uncaughtException', function (err) {
+  console.error(err.stack);
+});
