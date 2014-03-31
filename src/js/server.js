13b6f25a5e6f489dc3a4ee1773fe37cbeea3461d
volokaleks@yandex.ru <volokaleks@yandex.ru@25897825-9b42-e7f4-afcc-2c1af6338310>
2014-03-31 16:58:16 +0000

    fix moduleName array

--- src/js/server.js
+++ src/js/server.js
@@ -4,9 +4,11 @@ var ipcmodule = require('ipcModule');
 
 var server = new servermodule.ClientServerModule("MainServer");
 var app = new ipcmodule.IPCModule("App1");
-server.StartServer(8124);
-//server.StartAsCoordinator();
+var app1 = new ipcmodule.IPCModule("App1");
+//server.StartServer(8124);
+server.StartAsCoordinator();
 app.Start();
+app1.Start();
 
 process.on('uncaughtException', function (err) {
   console.error(err.stack);
