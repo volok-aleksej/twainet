c84e39fc79e40d94958a6d9fa58a053f79460411
Alexey <volok-aleksej@yandex.ru>
2017-01-18 17:19:13 +0200

    thread manager uses tasks

--- src/Arduino/twainet/thread_manager.cpp
+++ src/Arduino/twainet/thread_manager.cpp
@@ -4,8 +4,36 @@
 
 unsigned int g_threadCount = 0;
 
+static uint32_t g_current_threadId = 0;
+static ThreadDescription g_threadDesks[THREAD_MAX];
+
+void thread_wrapper()
+{
+    ThreadDescription& desk = g_threadDesks[g_current_threadId];
+    ThreadManager::GetInstance().StartThread(desk.m_thread);
+}
+
+static void thread_func(os_event_t *events)
+{
+    ThreadDescription& desk = g_threadDesks[events->sig];
+    g_current_threadId = events->sig;
+    desk.m_state = ThreadDescription::RUNNING;
+    cont_run(&desk.m_cont, &thread_wrapper);
+    if (cont_check(&desk.m_cont) != 0) {
+        panic();
+    }
+}
+
 ThreadManager::ThreadManager()
 {
+    for(uint8_t i = 0; i < THREAD_MAX; i++) {
+        g_threadDesks[i].m_id = THREAD_START_ID + i;
+        g_threadDesks[i].m_thread = 0;
+        g_threadDesks[i].m_state = ThreadDescription::ABSENT;
+        if(!ets_task(thread_func, g_threadDesks[i].m_id, g_threadDesks[i].m_loop_queue, QUEUE_SIZE)) {
+            panic();
+        }
+    }
 }
 
 ThreadManager::~ThreadManager()
@@ -14,26 +42,36 @@ ThreadManager::~ThreadManager()
 
 void ThreadManager::AddThread(Thread* thread)
 {
-    m_threads.insert(m_threads.end(), thread);
+    for(uint8_t i = 0; i < THREAD_MAX; i++) {
+        if(g_threadDesks[i].m_state == ThreadDescription::ABSENT) {
+            g_threadDesks[i].m_thread = thread;
+            g_threadDesks[i].m_state = ThreadDescription::CREATED;
+            return;
+        }
+    }
+    
+    panic();
 }
 
 void ThreadManager::RemoveThread(Thread* thread)
 {
-    for(twnstd::list<Thread*>::iterator it = m_threads.begin();
-        it != m_threads.end(); ++it)
-    {
-        if(*it == thread)
-        {
-            (*it)->StopThread();
-            delete *it;
-            m_threads.erase(it);
+    for(uint8_t i = 0; i < THREAD_MAX; i++) {
+        if(g_threadDesks[i].m_thread == thread) {
+            g_threadDesks[i].m_thread->StopThread();
+            delete g_threadDesks[i].m_thread;
+            g_threadDesks[i].m_state = ThreadDescription::ABSENT;
             break;
         }
     }
 }
 
-void ThreadManager::CheckThreads()
+void ThreadManager::StartThread(Thread* thread)
 {
+    thread->ThreadFunc();
+}
+
+void ThreadManager::CheckThreads()
+{/*
     for(twnstd::list<Thread*>::iterator it = m_threads.begin(); it != m_threads.end(); ++it)
     {
         if((*it)->IsRunning()) {
@@ -42,5 +80,5 @@ void ThreadManager::CheckThreads()
             delete *it;
             it = m_threads.erase(it);
         }
-    }
+    }*/
 }
--- src/Arduino/twainet/thread_manager.h
+++ src/Arduino/twainet/thread_manager.h
@@ -4,6 +4,35 @@
 #include "thread.h"
 #include "include/singleton.h"
 #include "std/list.hpp"
+#include "std/vector.hpp"
+
+extern "C" {
+#include <ets_sys.h>
+#include <os_type.h>
+#include <cont.h>
+#include <debug.h>
+}
+    
+#define THREAD_START_ID 2
+#define QUEUE_SIZE      1
+#define THREAD_MAX      5
+
+struct ThreadDescription
+{
+    cont_t m_cont __attribute__ ((aligned (16)));
+    os_event_t m_loop_queue[QUEUE_SIZE];
+    unsigned int m_id;
+    Thread* m_thread;
+    enum {
+        ABSENT,
+        CREATED,
+        START_PENDING,
+        RUNNING,
+        WAITING,
+        STOPPED,
+        STOP_PENDING
+    } m_state;
+};
 
 class ThreadManager : public Singleton<ThreadManager>
 {
@@ -14,10 +43,10 @@ protected:
 public:
 	void AddThread(Thread* thread);
 	void RemoveThread(Thread* thread);
-    
+protected:
+    friend void thread_wrapper();
+    void StartThread(Thread* thread);
     void CheckThreads();
-private:
-	twnstd::list<Thread*> m_threads;
 };
 
 #endif/*THREAD_MANAGER_H*/
